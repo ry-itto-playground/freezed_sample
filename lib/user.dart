b4ce17a0b67f387bf80430913f95336c26449167
ryoya ito <110ryoya@gmail.com>
2020-12-21 10:46:36 +0900

    init

---
+++ lib/user.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
+@freezed
+abstract class User with _$User {
+  const factory User({
+    String name,
+    int age,
+  }) = _User;
+}
